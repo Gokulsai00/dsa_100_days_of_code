2412990d6b51e580e28d2746d152bd265ba952ee
Gokulsai00 <challagokul00@gmail.com>
2026-02-24 16:35:53 +0530

    add

---
+++ day24.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node* head, int value) {
+    struct Node* newNode = createNode(value);
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* deleteKey(struct Node* head, int key) {
+
+    if (head == NULL)
+        return head;
+
+    struct Node* temp = head;
+    struct Node* prev = NULL;
+
+    if (temp->data == key) {
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+        return head;
+
+    prev->next = temp->next;
+    free(temp);
+
+    return head;
+}
+
+void display(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    struct Node* head = NULL;
+    int n, value, key;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        head = insertEnd(head, value);
+    }
+
+    scanf("%d", &key);
+
+    head = deleteKey(head, key);
+
+    display(head);
+
+    return 0;
+}
